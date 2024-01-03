4c527039200ea21c989c0cf85b21d08a40b489d8
pit1107 <minhphatnmp@gmail.com>
2024-01-04 02:22:04 +0700

    video 16

---
+++ src/components/Mycomponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Mycomponent extends React.Component {
+
+    //JSX
+    render() {
+        return (
+            <div>My first component</div>
+        );
+    }
+}
+
+export default Mycomponent;
