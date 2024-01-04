194ac663e7f20ec9f0d9ebb5a94fc6ec0d1fa8e5
pit1107 <minhphatnmp@gmail.com>
2024-01-05 00:30:01 +0700

    bai 20

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -8,16 +8,32 @@ class Mycomponent extends React.Component {
         age: 22
     };
 
+
+
+    handleClick = (event) => {
+        console.log('Xin chao: ', this.state.name);
+        // console.log(event);
+        this.setState({
+            name: 'Nguyen Phat',
+            age: (this.state.age + 1)
+        });
+    }
+
+    handleOnMoverOver(event) {
+        console.log(event.pageX);
+    }
     //JSX
     render() {
         return (
             <div>
                 My first component
-                
-                My name is {this.state.name} and I'm from {this.state.address}
+
+                My name is {this.state.name} and I'm {this.state.age}
+                <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
             </div>
         );
     }
 }
 
-export default Mycomponent;
+export default Mycomponent; 
